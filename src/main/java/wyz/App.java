2461e61d2189ed4a4be95bdd704d252b2e5da392
wangyunzhong123 <1337478765@qq.com>
2016-07-29 21:17:25 +0800

    Initial commit

---
+++ src/main/java/wyz/App.java
@@ -0,0 +1,17 @@
+package wyz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void sayHello(){
+        System.out.println( "Hello World!!" );
+    }
+}
