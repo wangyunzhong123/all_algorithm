e76b236c9b7a0df4912526c4c9bbab3d7eeaa771
wangyunzhong123 <1337478765@qq.com>
2019-07-26 10:17:53 +0800

    ADD: java8

--- src/main/java/Netty/Websocket/server/netty/NettyConfig.java
+++ src/main/java/Netty/Websocket/server/netty/NettyConfig.java
@@ -4,6 +4,8 @@ import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
+import java.util.function.Predicate;
+
 /**
  * Created by yzwang on 2019/7/10.
  */
@@ -11,5 +13,7 @@ public class NettyConfig {
 
     //存储每一个客户端接入进来时的channel对象
     public static ChannelGroup group = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    Predicate p;
 
 }
