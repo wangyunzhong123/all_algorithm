6d955de374ea2f3560ae6a54ee4ba8837c255a9a
wangyunzhong123 <1337478765@qq.com>
2019-07-26 13:45:45 +0800

    Revert "ADD: java8"
    This reverts commit e76b236c9b7a0df4912526c4c9bbab3d7eeaa771.

--- src/main/java/Netty/Websocket/server/netty/NettyConfig.java
+++ src/main/java/Netty/Websocket/server/netty/NettyConfig.java
@@ -4,8 +4,6 @@ import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
-import java.util.function.Predicate;
-
 /**
  * Created by yzwang on 2019/7/10.
  */
@@ -13,7 +11,5 @@ public class NettyConfig {
 
     //存储每一个客户端接入进来时的channel对象
     public static ChannelGroup group = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
-
-    Predicate p;
 
 }
